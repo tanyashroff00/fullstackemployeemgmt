2c21d942f424c72a99ad0581d1f9bf1bef8ec23c
Tanya Shroff <tanyashroff00@gmail.com>
2024-08-05 07:27:58 +0000

    comment added by developer

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,7 @@ sap.ui.define(
 
     return BaseController.extend("employeemanagement.controller.App", {
       onInit: function () {
+        // developed by tanya shroff
       },
 
       getRouter: function () {
